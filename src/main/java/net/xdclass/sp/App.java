6ac4c4e40957102e7348a11cf17c334d311e39d4
cliz <geynp123@hotmail.com>
2022-11-14 17:56:02 +0800

    20221114

--- src/main/java/net/xdclass/sp/App.java
+++ src/main/java/net/xdclass/sp/App.java
@@ -1,15 +1,49 @@
 package net.xdclass.sp;
 
 import net.xdclass.sp.domain.Video;
+import net.xdclass.sp.domain.Video2;
+import net.xdclass.sp.domain.VideoOrder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Video video = (Video)applicationContext.getBean("video");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        testScope(context);
+//        testInject(context);
+//        testInjectCollection(context);
+        testInjecta(context);
 
+    }
+
+
+    private static void testInjecta(ApplicationContext context) {
+        Video video = (Video)context.getBean("video");
         System.out.println(video.getTitle());
+
+        Video2 video2 = (Video2)context.getBean("video2");
+        System.out.println(video2.getSummary());
+
+    }
+
+    private static void testInjectCollection(ApplicationContext context) {
+        Video video = (Video) context.getBean("video");
+//        System.out.println(video.getChapterList().toString());
+//        System.out.println(video.getVideoMap().values().toString());
+    }
+    private static void testInject(ApplicationContext context) {
+        Video video = (Video)context.getBean("video");
+        System.out.println(video.getTitle());
+
+        VideoOrder videoOrder = (VideoOrder) context.getBean("videoOrder");
+        System.out.println(videoOrder.getVideo().getTitle());
+    }
+
+    private static void testScope(ApplicationContext context){
+        Video video1 = (Video)context.getBean("video");
+        Video video2 = (Video)context.getBean("video");
+        //øø∆•≈‰ƒ⁄¥Êµÿ÷∑£¨ ==  «∆•≈‰ƒ⁄¥Êµÿ÷∑
+        System.out.println(video1==video2);
     }
 }
--- src/main/java/net/xdclass/sp/domain/Video.java
+++ src/main/java/net/xdclass/sp/domain/Video.java
@@ -1,14 +1,28 @@
 package net.xdclass.sp.domain;
 
+import java.util.List;
+import java.util.Map;
+
 public class Video {
     private int id;
     private String title;
+
+
+    public Video(){
+
+    }
+    public Video(String title){
+        System.out.println("video 带参构造函数被调用");
+        this.title=title;
+    }
 
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
+        System.out.println("Video setId方法被调用");
+
         this.id = id;
     }
 
@@ -17,6 +31,7 @@ public class Video {
     }
 
     public void setTitle(String title) {
+        System.out.println("Video setTitle方法被调用");
         this.title = title;
     }
 }
